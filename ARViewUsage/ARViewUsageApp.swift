d5ffb89f0b4dab131889266b69d1cc5897a956bc
farkhod khaknazarov <f.khaknazarov@avtoapp.ru>
2023-03-31 22:54:40 +0300

    Initial Commit

---
+++ ARViewUsage/ARViewUsageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARViewUsageApp.swift
+//  ARViewUsage
+//
+//  Created by Farkhod Kh on 31.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ARViewUsageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
